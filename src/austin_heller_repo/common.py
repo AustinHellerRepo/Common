fdd858b4c33beab31258a5837040b34d6f431a3f
austin <galaky2005@gmail.com>
2021-12-09 17:20:06 -0500

    added HostPointer class;

--- src/austin_heller_repo/common.py
+++ src/austin_heller_repo/common.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from enum import Enum
 from typing import List, Tuple, Dict
+from abc import ABC, abstractmethod
 
 
 class StringEnum(Enum):
@@ -14,3 +15,17 @@ class StringEnum(Enum):
 		for enum_list_element in enum_list:
 			string_list.append(enum_list_element.value)
 		return string_list
+
+
+class HostPointer():
+
+	def __init__(self, *, host_address: str, host_port: int):
+
+		self.__host_address = host_address
+		self.__host_port = host_port
+
+	def get_host_address(self) -> str:
+		return self.__host_address
+
+	def get_host_port(self) -> int:
+		return self.__host_port
