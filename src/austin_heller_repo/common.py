eb948aa966b93b24a05bf9ce5d2e5908491531ef
austin <galaky2005@gmail.com>
2022-03-20 13:07:08 -0400

    testing clipboard fix;

--- src/austin_heller_repo/common.py
+++ src/austin_heller_repo/common.py
@@ -411,6 +411,7 @@ def copy_to_clipboard(*, text: str):
 		instance.withdraw()
 		instance.clipboard_clear()
 		instance.clipboard_append(text)
+		instance.mainloop()
 	finally:
 		instance.destroy()
 
@@ -420,6 +421,7 @@ def paste_from_clipboard() -> str:
 	try:
 		instance.withdraw()
 		text = instance.clipboard_get()
+		instance.mainloop()
 	finally:
 		instance.destroy()
 	return text
