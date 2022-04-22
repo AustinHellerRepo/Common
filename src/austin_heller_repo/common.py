7d3aab3d52da77aed2f65216061a62768e3563eb
austin <galaky2005@gmail.com>
2022-04-22 19:25:49 -0400

    trying to determine why files do not appear to be deleted;

--- src/austin_heller_repo/common.py
+++ src/austin_heller_repo/common.py
@@ -369,7 +369,9 @@ def is_directory_empty(*, directory_path: str) -> bool:
 def delete_directory_contents(directory_path: str):
 	for file_name in os.listdir(directory_path):
 		file_path = os.path.join(directory_path, file_name)
-		if os.path.isfile(file_path) or os.path.islink(file_path):
+		if os.path.isfile(file_path):
+			os.remove(file_path)
+		elif os.path.islink(file_path):
 			os.unlink(file_path)
 		elif os.path.isdir(file_path):
 			shutil.rmtree(file_path)
